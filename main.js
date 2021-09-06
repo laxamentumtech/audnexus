7f1142524495a568a447201976499dbf8683987e
djdembeck <dembeck.david@protonmail.com>
2021-09-06 15:59:42 -0500

    Setup as modules for now

---
+++ main.js
@@ -0,0 +1,23 @@
+const apiHelper = require("./plugins/audibleApiHelper")
+const scrapeHelper = require("./plugins/audibleScrapeHelper")
+
+// For now, testing uses static asin
+let ASIN = "0593103629";
+
+/**
+ *
+ * @returns (0) JSON api resonse, (1) JSON with series and genres.
+ */
+async function fetchData() {
+    const api = new apiHelper(ASIN)
+    const scraper = new scrapeHelper(ASIN)
+
+	// Fetch both api and html at same time
+	const listOfPromises = [api.fetchBook(), scraper.fetchBook()];
+	return await Promise.all(listOfPromises).then((res) => {
+		console.log(res);
+		return res;
+	});
+}
+
+fetchData();
