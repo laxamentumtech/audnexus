626dc2e7963ce24b948fc51165bb697597c9a5d0
djdembeck <dembeck.david@protonmail.com>
2021-09-06 18:28:19 -0500

    Add in Molecular

---
+++ services/book.service.js
@@ -0,0 +1,76 @@
+"use strict";
+const apiHelper = require("../plugins/audibleApiHelper");
+const scrapeHelper = require("../plugins/audibleScrapeHelper");
+const stitchHelper = require("../plugins/audibleStitchHelper");
+/**
+ * @typedef {import('moleculer').Context} Context Moleculer's Context
+ */
+
+module.exports = {
+    name: "book",
+
+    /**
+     * Settings
+     */
+    settings: {},
+
+    /**
+     * Dependencies
+     */
+    dependencies: [],
+
+    /**
+     * Actions
+     */
+    actions: {
+        getBook: {
+            rest: {
+                method: "GET",
+                path: "/",
+            },
+            params: {
+                asin: "string",
+            },
+            cache: {
+                ttl: 86400,
+            },
+            /** @param {Context} ctx  */
+            async handler(ctx) {
+                const api = new apiHelper(ctx.params.asin);
+                const scraper = new scrapeHelper(ctx.params.asin);
+
+                // Fetch both api and html at same time
+                const listOfPromises = [api.fetchBook(), scraper.fetchBook()];
+                return await Promise.all(listOfPromises).then((res) => {
+                    const stitch = new stitchHelper(res[0], res[1]);
+                    return stitch.process();
+                });
+            },
+        },
+    },
+
+    /**
+     * Events
+     */
+    events: {},
+
+    /**
+     * Methods
+     */
+    methods: {},
+
+    /**
+     * Service created lifecycle event handler
+     */
+    created() {},
+
+    /**
+     * Service started lifecycle event handler
+     */
+    async started() {},
+
+    /**
+     * Service stopped lifecycle event handler
+     */
+    async stopped() {},
+};
