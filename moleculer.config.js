c405059339edc42acd41298e89930d0572a853f2
djdembeck <dembeck.david@protonmail.com>
2021-09-06 19:27:10 -0500

    This should actually do it

--- moleculer.config.js
+++ moleculer.config.js
@@ -62,7 +62,14 @@ module.exports = {
 
     // Define a cacher.
     // More info: https://moleculer.services/docs/0.14/caching.html
-    cacher: `redis://${process.env.REDIS_URL}`,
+    // cacher: "Redis",
+    cacher: {
+        type: "Redis",
+
+        redis: {
+            host: process.env.REDIS_URL,
+        },
+    },
 
     // Define a serializer.
     // Available values: "JSON", "Avro", "ProtoBuf", "MsgPack", "Notepack", "Thrift".
