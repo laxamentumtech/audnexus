febda35e0629d3f3a6bd62d093726fc777d89f03
djdembeck <dembeck.david@protonmail.com>
2021-09-08 03:41:09 -0500

    don't use localhost

--- src/server.ts
+++ src/server.ts
@@ -3,13 +3,14 @@ import bookRoute from './config/routes/books'
 
 // Heroku or local port
 const Port = process.env.PORT || 3000
+const host = '0.0.0.0'
 const server = fastify({
     logger: true
 })
 
 server.register(bookRoute)
 
-server.listen(Port, (err, address) => {
+server.listen(Port, host, (err, address) => {
     if (err) {
         console.error(err)
         process.exit(1)
