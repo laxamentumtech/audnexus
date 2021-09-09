e4ae081a569d606cf01e876e8ac94618ba44de10
djdembeck <dembeck.david@protonmail.com>
2021-09-09 14:51:39 -0500

    Check ASIN length

--- src/helpers/shared.ts
+++ src/helpers/shared.ts
@@ -1,5 +1,10 @@
 class SharedHelper {
     checkAsinValidity (asin: string): boolean {
+        // First things first, check length
+        if (asin.length !== 10) {
+            return false
+        }
+        // Check ASIN structure
         const asinRegex = /[0-9A-Z]{10}/gm
 
         const match = asin.match(asinRegex)
