6bac824bdc9fafd94079d34e31622f54fc6ebdbd
djdembeck <dembeck.david@protonmail.com>
2021-09-20 22:32:15 -0500

    refactor: :recycle: use redis url not host

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { connect } from './config/papr'
 // Heroku or local port
 const Port = process.env.PORT || 3000
 const host = '0.0.0.0'
-const REDIS_URL = process.env.REDIS_URL || '127.0.0.1'
+const REDIS_URL = process.env.REDIS_URL || 'redis://127.0.0.1'
 const server = fastify({
     logger: {
         level: 'warn'
@@ -17,7 +17,7 @@ const server = fastify({
 server.register(showBook)
 server.register(showChapter)
 server.register(deleteBook)
-server.register(require('fastify-redis'), { host: REDIS_URL })
+server.register(require('fastify-redis'), { url: REDIS_URL })
 
 server.listen(Port, host, async (err, address) => {
     if (err) {
