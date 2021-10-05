92dec9208cded2776860673de0c15bbdc073c1ce
djdembeck <dembeck.david@protonmail.com>
2021-10-04 23:12:02 -0500

    feat(author-search): :recycle: use FlexSearch to return author results

--- src/config/routes/authors/search/show.ts
+++ src/config/routes/authors/search/show.ts
@@ -1,33 +1,46 @@
 import SharedHelper from '../../../../helpers/shared'
 import Author from '../../../models/Author'
+// Search
+import { Document } from 'flexsearch'
+
+const index = new Document({
+    document: {
+        id: 'asin',
+        index: ['name']
+    },
+    preset: 'score'
+})
 
 async function routes (fastify, options) {
     fastify.get('/authors', async (request, reply) => {
-        const asin = request.query.asin
         const name = request.query.name
 
-        if (!name && !asin) {
+        if (!name) {
             throw new Error('No search params provided')
-        }
-
-        if (asin) {
-            // First, check ASIN validity
-            const commonHelpers = new SharedHelper()
-            if (!commonHelpers.checkAsinValidity(asin)) {
-                throw new Error('Bad ASIN')
-            }
-            return Promise.resolve(Author.findOne({ asin: asin }))
         }
 
         // Use regular text search until weighted available:
         // https://github.com/plexinc/papr/issues/98
         if (name) {
-            return Promise.resolve(
+            // Find all results of name
+            const seardDbByName = await Promise.resolve(
                 Author.find(
-                    { $text: { $search: `"${name}"` } },
-                    { limit: 25 }
+                    { $text: { $search: name } }
                 )
             )
+            // Add results to FlexSearch index
+            seardDbByName.forEach(result => {
+                index.add(result)
+            })
+            // Resulting search
+            const matchedResults = index.search(name)[0].result as string[]
+            // Search documents matched by FlexSearch
+            const found = await Promise.resolve(
+                Author.find(
+                    { asin: { $in: matchedResults } }
+                )
+            )
+            return found
         }
     })
 }
