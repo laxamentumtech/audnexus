26435e15cbf8a1457438519a1640c4ab085eee13
djdembeck <dembeck.david@protonmail.com>
2021-10-14 12:40:25 -0500

    fix(author-search): :recycle: remove flexsearch and just use mongodb search
    Since now it scores properly, this is a more reliable solution. Flexsearch was sometimes returning no results when it should have been
    
    This may cause search to behave much differently; clients should be scoring themselves already though

--- src/config/routes/authors/search/show.ts
+++ src/config/routes/authors/search/show.ts
@@ -1,18 +1,4 @@
 import Author from '../../../models/Author'
-// Search
-import { Document } from 'flexsearch'
-
-const document = new Document({
-    document: {
-        id: 'asin',
-        index: [{
-            charset: 'latin:simple',
-            field: 'name',
-            tokenize: 'full'
-        }]
-    },
-    preset: 'score'
-})
 
 async function routes (fastify, options) {
     fastify.get('/authors', async (request, reply) => {
@@ -29,28 +15,10 @@ async function routes (fastify, options) {
             const searchDbByName = await Promise.resolve(
                 Author.find(
                     { $text: { $search: name } },
-                    { projection: { _id: false, asin: true, name: true }, limit: 25, sort: { score: { $meta: 'textScore' } } },
+                    { projection: { _id: false, asin: true, name: true }, limit: 25, sort: { score: { $meta: 'textScore' } } }
                 )
             )
-            // Add results to FlexSearch index
-            searchDbByName.forEach(result => {
-                document.add(result)
-            })
-            // Resulting search
-            const runFlexSearch = document.search(name)
-            if (runFlexSearch.length) {
-                const matchedResults = runFlexSearch[0].result as string[]
-                // Search documents matched by FlexSearch
-                const found = await Promise.resolve(
-                    Author.find(
-                        { asin: { $in: matchedResults } }
-                    )
-                )
-                if (found) {
-                    return found
-                }
-            }
-            return []
+            return searchDbByName
         }
     })
 }
