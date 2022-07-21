6f015b92c66755ba58cfa7e3c791e66f3cd1b374
djdembeck <dembeck.david@protonmail.com>
2022-07-21 17:59:05 -0500

    refactor(schema): :card_file_box: add timestamps; align schema required fields with interface required fields
    BREAKING! Previously created items will need to be updated with `update=2` requests

--- src/config/models/Chapter.ts
+++ src/config/models/Chapter.ts
@@ -1,23 +1,26 @@
 import { types, schema } from 'papr'
 import papr from '#config/papr'
 
-const chapterSchema = schema({
-    asin: types.string({ required: true }),
-    brandIntroDurationMs: types.number({ required: true }),
-    brandOutroDurationMs: types.number({ required: true }),
-    chapters: types.array(
-        types.object({
-            lengthMs: types.number({ required: true }),
-            startOffsetMs: types.number({ required: true }),
-            startOffsetSec: types.number({ required: true }),
-            title: types.string({ required: true })
-        }),
-        { required: true }
-    ),
-    isAccurate: types.boolean({ required: true }),
-    runtimeLengthMs: types.number({ required: true }),
-    runtimeLengthSec: types.number({ required: true })
-})
+const chapterSchema = schema(
+    {
+        asin: types.string({ required: true }),
+        brandIntroDurationMs: types.number({ required: true }),
+        brandOutroDurationMs: types.number({ required: true }),
+        chapters: types.array(
+            types.object({
+                lengthMs: types.number({ required: true }),
+                startOffsetMs: types.number({ required: true }),
+                startOffsetSec: types.number({ required: true }),
+                title: types.string({ required: true })
+            }),
+            { required: true }
+        ),
+        isAccurate: types.boolean({ required: true }),
+        runtimeLengthMs: types.number({ required: true }),
+        runtimeLengthSec: types.number({ required: true })
+    },
+    { timestamps: true }
+)
 
 export type ChapterDocument = typeof chapterSchema[0]
 const Chapter = papr.model('chapters', chapterSchema)
