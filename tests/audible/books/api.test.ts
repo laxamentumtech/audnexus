f1cf23c1c07082c271747a66832a8b19790d94e8
djdembeck <dembeck.david@protonmail.com>
2022-08-02 16:35:09 -0500

    test(helper): :white_check_mark: improve layout of api tests

--- tests/audible/books/api.test.ts
+++ tests/audible/books/api.test.ts
@@ -67,77 +67,78 @@ function setupMinimalParsed(
 	}
 }
 
-// Live api testing
-describe('When fetching Project Hail Mary from Audible API', () => {
-	let response: AudibleProduct['product']
-	beforeAll((done) => {
-		apiGood = new ApiHelper('B08G9PRS1K')
-		apiGood.fetchBook().then((result) => {
-			if (!result) return undefined
-			response = result.product
-			// Make an object with the same keys as the response
-			minimalResponse = setupMinimalResponse(response)
-			done()
+describe('Audible API', () => {
+	describe('When fetching Project Hail Mary from Audible API', () => {
+		let response: AudibleProduct['product']
+		beforeAll((done) => {
+			apiGood = new ApiHelper('B08G9PRS1K')
+			apiGood.fetchBook().then((result) => {
+				if (!result) return undefined
+				response = result.product
+				// Make an object with the same keys as the response
+				minimalResponse = setupMinimalResponse(response)
+				done()
+			})
+		})
+
+		it('returned the correct data', () => {
+			expect(minimalResponse).toEqual(minimalB08G9PRS1K)
 		})
 	})
 
-	it('returned the correct data', () => {
-		expect(minimalResponse).toEqual(minimalB08G9PRS1K)
-	})
-})
+	describe('When fetching The Coldest Case from Audible API', () => {
+		let response: AudibleProduct['product']
+		beforeAll((done) => {
+			apiGood = new ApiHelper('B08C6YJ1LS')
+			apiGood.fetchBook().then((result) => {
+				if (!result) return undefined
+				response = result.product
+				// Make an object with the same keys as the response
+				minimalResponse = setupMinimalResponse(response)
+				done()
+			})
+		})
 
-describe('When fetching The Coldest Case from Audible API', () => {
-	let response: AudibleProduct['product']
-	beforeAll((done) => {
-		apiGood = new ApiHelper('B08C6YJ1LS')
-		apiGood.fetchBook().then((result) => {
-			if (!result) return undefined
-			response = result.product
-			// Make an object with the same keys as the response
-			minimalResponse = setupMinimalResponse(response)
-			done()
+		it('returned the correct data', () => {
+			expect(minimalResponse).toEqual(minimalB08C6YJ1LS)
 		})
 	})
 
-	it('returned the correct data', () => {
-		expect(minimalResponse).toEqual(minimalB08C6YJ1LS)
-	})
-})
+	describe('When parsing The Coldest Case', () => {
+		let response: ApiBook
+		beforeAll(async () => {
+			apiGood = new ApiHelper('B08C6YJ1LS')
+			const fetched = await apiGood.fetchBook()
+			const parsed = await apiGood.parseResponse(fetched)
+			response = parsed
+			// Make an object with the same keys as the response
+			const description =
+				"James Patterson's Detective Billy Harney is back, this time investigating murders in a notorious Chicago drug ring, which will lead him, his sister, and his new partner through a dangerous web of corrupt politicians, vengeful billionaires, and violent dark web conspiracies...."
+			const image = 'https://m.media-amazon.com/images/I/91H9ynKGNwL.jpg'
+			minimalParsed = setupMinimalParsed(B08C6YJ1LS.product, description, image)
+		})
 
-describe('When parsing The Coldest Case', () => {
-	let response: ApiBook
-	beforeAll(async () => {
-		apiGood = new ApiHelper('B08C6YJ1LS')
-		const fetched = await apiGood.fetchBook()
-		const parsed = await apiGood.parseResponse(fetched)
-		response = parsed
-		// Make an object with the same keys as the response
-		const description =
-			"James Patterson's Detective Billy Harney is back, this time investigating murders in a notorious Chicago drug ring, which will lead him, his sister, and his new partner through a dangerous web of corrupt politicians, vengeful billionaires, and violent dark web conspiracies...."
-		const image = 'https://m.media-amazon.com/images/I/91H9ynKGNwL.jpg'
-		minimalParsed = setupMinimalParsed(B08C6YJ1LS.product, description, image)
-	})
-
-	it('returned the correct data', () => {
-		expect(response).toEqual(minimalParsed)
-	})
-})
-
-describe('When parsing Scorcerers Stone', () => {
-	let response: ApiBook
-	beforeAll(async () => {
-		apiGood = new ApiHelper('B017V4IM1G')
-		const fetched = await apiGood.fetchBook()
-		const parsed = await apiGood.parseResponse(fetched)
-		response = parsed
-		// Make an object with the same keys as the response
-		const description =
-			'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle....'
-		const image = 'https://m.media-amazon.com/images/I/91eopoUCjLL.jpg'
-		minimalParsed = setupMinimalParsed(B017V4IM1G.product, description, image)
+		it('returned the correct data', () => {
+			expect(response).toEqual(minimalParsed)
+		})
 	})
 
-	it('returned the correct data', () => {
-		expect(response).toEqual(minimalParsed)
+	describe('When parsing Scorcerers Stone', () => {
+		let response: ApiBook
+		beforeAll(async () => {
+			apiGood = new ApiHelper('B017V4IM1G')
+			const fetched = await apiGood.fetchBook()
+			const parsed = await apiGood.parseResponse(fetched)
+			response = parsed
+			// Make an object with the same keys as the response
+			const description =
+				'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle....'
+			const image = 'https://m.media-amazon.com/images/I/91eopoUCjLL.jpg'
+			minimalParsed = setupMinimalParsed(B017V4IM1G.product, description, image)
+		})
+
+		it('returned the correct data', () => {
+			expect(response).toEqual(minimalParsed)
+		})
 	})
 })
--- tests/helpers/books/audible/ApiHelper.test.ts
+++ tests/helpers/books/audible/ApiHelper.test.ts
@@ -18,100 +18,132 @@ afterEach(() => {
 	jest.restoreAllMocks()
 })
 
-test('should setup constructor correctly', () => {
-	expect(helper.asin).toBe('B079LRSMNN')
-	expect(helper.reqUrl).toBe(
-		'https://api.audible.com/1.0/catalog/products/B079LRSMNN/?response_groups=contributors,product_desc,product_extended_attrs,product_attrs,media,rating,series&image_sizes=500,1024'
-	)
-})
+describe('ApiHelper should', () => {
+	test('setup constructor correctly', () => {
+		expect(helper.asin).toBe('B079LRSMNN')
+		expect(helper.reqUrl).toBe(
+			'https://api.audible.com/1.0/catalog/products/B079LRSMNN/?response_groups=contributors,product_desc,product_extended_attrs,product_attrs,media,rating,series&image_sizes=500,1024'
+		)
+	})
 
-test.todo('should check required keys')
+	test.todo('check required keys')
 
-test('should throw error if no input data', () => {
-	helper.inputJson = undefined
-	expect(() => helper.getHighResImage()).toThrowError('No input data')
-	expect(() => helper.getReleaseDate()).toThrowError('No input data')
-	expect(() => helper.getSeriesPrimary(apiResponse.product.series)).toThrowError('No input data')
-	expect(() => helper.getSeriesSecondary(apiResponse.product.series)).toThrowError('No input data')
-	expect(() => helper.getFinalData()).toThrowError('No input data')
-})
+	test('get high res image', () => {
+		expect(helper.getHighResImage()).toBe('https://m.media-amazon.com/images/I/91spdScZuIL.jpg')
+	})
 
-test('should get high res image', () => {
-	expect(helper.getHighResImage()).toBe('https://m.media-amazon.com/images/I/91spdScZuIL.jpg')
-})
+	test('get release date', () => {
+		expect(helper.getReleaseDate()).toBeInstanceOf(Date)
+	})
 
-test('should get release date', () => {
-	expect(helper.getReleaseDate()).toBeInstanceOf(Date)
-})
+	test('get series', () => {
+		// Should return undefined if no series title
+		expect(helper.getSeries({ asin: '123', title: '', sequence: '1', url: '' })).toBeUndefined()
+		expect(helper.getSeries(apiResponse.product.series[0])).toEqual({
+			asin: 'B079YXK1GL',
+			name: "Galaxy's Edge Series",
+			position: '1-2'
+		})
+	})
 
-test('should get series', () => {
-	// Should return undefined if no series title
-	expect(helper.getSeries({ asin: '123', title: '', sequence: '1', url: '' })).toBeUndefined()
-	expect(helper.getSeries(apiResponse.product.series[0])).toEqual({
-		asin: 'B079YXK1GL',
-		name: "Galaxy's Edge Series",
-		position: '1-2'
+	test('get series primary', () => {
+		// Should return undefined if no series name
+		expect(
+			helper.getSeriesPrimary([{ asin: '123', title: '', sequence: '1', url: '' }])
+		).toBeUndefined()
+		expect(helper.getSeriesPrimary(apiResponse.product.series)).toEqual({
+			asin: 'B079YXK1GL',
+			name: "Galaxy's Edge Series",
+			position: '1-2'
+		})
+	})
+
+	test.todo('get series without position')
+
+	test('fetch book data', async () => {
+		jest.spyOn(ApiHelper.prototype, 'fetchBook').mockResolvedValue(apiResponse)
+		const data = await helper.fetchBook()
+		expect(data).toEqual(apiResponse)
+	})
+
+	test('parse response', async () => {
+		await expect(helper.parseResponse(apiResponse)).resolves.toEqual(parsedBook)
 	})
 })
 
-test('should get series primary', () => {
-	// Should return undefined if no series name
-	expect(
-		helper.getSeriesPrimary([{ asin: '123', title: '', sequence: '1', url: '' }])
-	).toBeUndefined()
-	expect(helper.getSeriesPrimary(apiResponse.product.series)).toEqual({
-		asin: 'B079YXK1GL',
-		name: "Galaxy's Edge Series",
-		position: '1-2'
+describe('ApiHelper edge cases should', () => {
+	test('get backup lower res image', () => {
+		helper.inputJson.product_images[1024] = undefined
+		expect(helper.getHighResImage()).toBe('https://m.media-amazon.com/images/I/51OIn2FgdtL.jpg')
+	})
+
+	test('handle no image', () => {
+		helper.inputJson.product_images = undefined
+		expect(helper.getHighResImage()).toBe(undefined)
+	})
+
+	test('use issue_date if release_date is not available', () => {
+		helper.inputJson.issue_date = helper.inputJson.release_date
+		helper.inputJson.release_date = undefined
+		expect(helper.getReleaseDate()).toBeInstanceOf(Date)
+	})
+
+	test('handle empty series array', () => {
+		expect(helper.getSeriesPrimary(undefined)).toBeUndefined()
+		expect(helper.getSeriesSecondary(undefined)).toBeUndefined()
+	})
+
+	test('parse a book with 2 series', async () => {
+		helper = new ApiHelper('B017V4IM1G')
+		const data = await helper.parseResponse(B017V4IM1G)
+		expect(data.seriesPrimary).toEqual({
+			asin: B017V4IM1G.product.series[0].asin,
+			name: B017V4IM1G.product.series[0].title,
+			position: B017V4IM1G.product.series[0].sequence
+		})
+		expect(data.seriesSecondary).toEqual({
+			asin: B017V4IM1G.product.series[1].asin,
+			name: B017V4IM1G.product.series[1].title,
+			position: B017V4IM1G.product.series[1].sequence
+		})
 	})
 })
 
-test.todo('should get series without position')
-
-test('should get final data', () => {
-	const data = helper.getFinalData()
-	expect(data).toEqual(parsedBook)
-})
-
-test('should fetch book data', async () => {
-	jest.spyOn(ApiHelper.prototype, 'fetchBook').mockResolvedValue(apiResponse)
-	const data = await helper.fetchBook()
-	expect(data).toEqual(apiResponse)
-})
-
-test('should throw error if error fetching book data', async () => {
-	helper = new ApiHelper('')
-	await expect(helper.fetchBook()).rejects.toThrowError(
-		'An error has occured while fetching from Audible API. Response: 403, ASIN: '
-	)
-})
-
-test('should parse response', async () => {
-	// Should throw error if input is undefined
-	await expect(helper.parseResponse(undefined)).rejects.toThrowError('No API response to parse')
-
-	await expect(helper.parseResponse(apiResponse)).resolves.toEqual(parsedBook)
-})
-
-// Edge cases
-test('should throw error when book has no title', async () => {
-	helper = new ApiHelper('B07BS4RKGH')
-	await expect(helper.parseResponse(B07BS4RKGH)).rejects.toThrowError(
-		'Required key: title, does not exist on: B07BS4RKGH'
-	)
-})
-
-test('should parse a book with 2 series', async () => {
-	helper = new ApiHelper('B017V4IM1G')
-	const data = await helper.parseResponse(B017V4IM1G)
-	expect(data.seriesPrimary).toEqual({
-		asin: B017V4IM1G.product.series[0].asin,
-		name: B017V4IM1G.product.series[0].title,
-		position: B017V4IM1G.product.series[0].sequence
+describe('ApiHelper should throw error when', () => {
+	test('no input data', () => {
+		helper.inputJson = undefined
+		expect(() => helper.getHighResImage()).toThrowError('No input data')
+		expect(() => helper.getReleaseDate()).toThrowError('No input data')
+		expect(() => helper.getSeriesPrimary(apiResponse.product.series)).toThrowError('No input data')
+		expect(() => helper.getSeriesSecondary(apiResponse.product.series)).toThrowError(
+			'No input data'
+		)
+		expect(() => helper.getFinalData()).toThrowError('No input data')
 	})
-	expect(data.seriesSecondary).toEqual({
-		asin: B017V4IM1G.product.series[1].asin,
-		name: B017V4IM1G.product.series[1].title,
-		position: B017V4IM1G.product.series[1].sequence
+
+	test('release_date is in the future', () => {
+		helper.inputJson.release_date = '2080-01-01'
+		expect(() => helper.getReleaseDate()).toThrowError('Release date is in the future')
+	})
+
+	test('error fetching book data', async () => {
+		helper = new ApiHelper('')
+		await expect(helper.fetchBook()).rejects.toThrowError(
+			'An error has occured while fetching from Audible API. Response: 403, ASIN: '
+		)
+	})
+
+	test('input is undefined', async () => {
+		await expect(helper.parseResponse(undefined)).rejects.toThrowError('No API response to parse')
+	})
+
+	test('book has no title', async () => {
+		helper = new ApiHelper('B07BS4RKGH')
+		await expect(helper.parseResponse(B07BS4RKGH)).rejects.toThrowError(
+			'Required key: title, does not exist on: B07BS4RKGH'
+		)
+
+		helper.inputJson = B07BS4RKGH.product
+		expect(() => helper.getFinalData()).toThrowError('No title')
 	})
 })
