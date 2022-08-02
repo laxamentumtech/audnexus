47ec52c80eef7ce1496de10f8c854eb68efaede7
djdembeck <dembeck.david@protonmail.com>
2022-08-02 14:35:45 -0500

    test(helper): :white_check_mark: de-dupe live api test a bit

--- tests/audible/books/api.test.ts
+++ tests/audible/books/api.test.ts
@@ -14,6 +14,59 @@ let apiGood: ApiHelper
 let minimalResponse: MinimalResponse
 let minimalParsed: Book
 
+function setupMinimalResponse(response: AudibleProduct['product']): MinimalResponse {
+	return {
+		asin: response.asin,
+		authors: response.authors,
+		merchandising_summary: response.merchandising_summary,
+		format_type: response.format_type,
+		language: response.language,
+		narrators: response.narrators,
+		product_images: response.product_images,
+		publisher_name: response.publisher_name,
+		publisher_summary: response.publisher_summary,
+		release_date: response.release_date,
+		runtime_length_min: response.runtime_length_min,
+		title: response.title
+	}
+}
+
+function setupMinimalParsed(
+	response: AudibleProduct['product'],
+	description: string,
+	image: string
+): Book {
+	return {
+		asin: response.asin,
+		authors: response.authors,
+		description: description,
+		formatType: response.format_type,
+		language: response.language,
+		narrators: response.narrators,
+		image: image,
+		rating: response.rating.overall_distribution.display_average_rating.toString(),
+		publisherName: response.publisher_name,
+		summary: response.publisher_summary,
+		releaseDate: new Date(response.release_date),
+		runtimeLengthMin: response.runtime_length_min,
+		title: response.title,
+		...(response.series?.[0] && {
+			seriesPrimary: {
+				asin: response.series[0].asin,
+				name: response.series[0].title,
+				position: response.series[0].sequence
+			}
+		}),
+		...(response.series?.[1] && {
+			seriesSecondary: {
+				asin: response.series[1].asin,
+				name: response.series[1].title,
+				position: response.series[1].sequence
+			}
+		})
+	}
+}
+
 // Live api testing
 describe('When fetching Project Hail Mary from Audible API', () => {
 	let response: AudibleProduct['product']
@@ -23,20 +76,7 @@ describe('When fetching Project Hail Mary from Audible API', () => {
 			if (!result) return undefined
 			response = result.product
 			// Make an object with the same keys as the response
-			minimalResponse = {
-				asin: response.asin,
-				authors: response.authors,
-				merchandising_summary: response.merchandising_summary,
-				format_type: response.format_type,
-				language: response.language,
-				narrators: response.narrators,
-				product_images: response.product_images,
-				publisher_name: response.publisher_name,
-				publisher_summary: response.publisher_summary,
-				release_date: response.release_date,
-				runtime_length_min: response.runtime_length_min,
-				title: response.title
-			}
+			minimalResponse = setupMinimalResponse(response)
 			done()
 		})
 	})
@@ -54,20 +94,7 @@ describe('When fetching The Coldest Case from Audible API', () => {
 			if (!result) return undefined
 			response = result.product
 			// Make an object with the same keys as the response
-			minimalResponse = {
-				asin: response.asin,
-				authors: response.authors,
-				merchandising_summary: response.merchandising_summary,
-				format_type: response.format_type,
-				language: response.language,
-				narrators: response.narrators,
-				product_images: response.product_images,
-				publisher_name: response.publisher_name,
-				publisher_summary: response.publisher_summary,
-				release_date: response.release_date,
-				runtime_length_min: response.runtime_length_min,
-				title: response.title
-			}
+			minimalResponse = setupMinimalResponse(response)
 			done()
 		})
 	})
@@ -85,27 +112,10 @@ describe('When parsing The Coldest Case', () => {
 		const parsed = await apiGood.parseResponse(fetched)
 		response = parsed
 		// Make an object with the same keys as the response
-		minimalParsed = {
-			asin: B08C6YJ1LS.product.asin,
-			authors: B08C6YJ1LS.product.authors,
-			description:
-				"James Patterson's Detective Billy Harney is back, this time investigating murders in a notorious Chicago drug ring, which will lead him, his sister, and his new partner through a dangerous web of corrupt politicians, vengeful billionaires, and violent dark web conspiracies....",
-			formatType: B08C6YJ1LS.product.format_type,
-			language: B08C6YJ1LS.product.language,
-			narrators: B08C6YJ1LS.product.narrators,
-			image: 'https://m.media-amazon.com/images/I/91H9ynKGNwL.jpg',
-			rating: B08C6YJ1LS.product.rating.overall_distribution.display_average_rating.toString(),
-			publisherName: B08C6YJ1LS.product.publisher_name,
-			summary: B08C6YJ1LS.product.publisher_summary,
-			releaseDate: new Date(B08C6YJ1LS.product.release_date),
-			runtimeLengthMin: B08C6YJ1LS.product.runtime_length_min,
-			title: B08C6YJ1LS.product.title,
-			seriesPrimary: {
-				asin: B08C6YJ1LS.product.series[0].asin,
-				name: B08C6YJ1LS.product.series[0].title,
-				position: B08C6YJ1LS.product.series[0].sequence
-			}
-		}
+		const description =
+			"James Patterson's Detective Billy Harney is back, this time investigating murders in a notorious Chicago drug ring, which will lead him, his sister, and his new partner through a dangerous web of corrupt politicians, vengeful billionaires, and violent dark web conspiracies...."
+		const image = 'https://m.media-amazon.com/images/I/91H9ynKGNwL.jpg'
+		minimalParsed = setupMinimalParsed(B08C6YJ1LS.product, description, image)
 	})
 
 	it('returned the correct data', () => {
@@ -121,32 +131,10 @@ describe('When parsing Scorcerers Stone', () => {
 		const parsed = await apiGood.parseResponse(fetched)
 		response = parsed
 		// Make an object with the same keys as the response
-		minimalParsed = {
-			asin: B017V4IM1G.product.asin,
-			authors: B017V4IM1G.product.authors,
-			description:
-				'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle....',
-			formatType: B017V4IM1G.product.format_type,
-			language: B017V4IM1G.product.language,
-			narrators: B017V4IM1G.product.narrators,
-			image: 'https://m.media-amazon.com/images/I/91eopoUCjLL.jpg',
-			rating: B017V4IM1G.product.rating.overall_distribution.display_average_rating.toString(),
-			publisherName: B017V4IM1G.product.publisher_name,
-			summary: B017V4IM1G.product.publisher_summary,
-			releaseDate: new Date(B017V4IM1G.product.release_date),
-			runtimeLengthMin: B017V4IM1G.product.runtime_length_min,
-			title: B017V4IM1G.product.title,
-			seriesPrimary: {
-				asin: B017V4IM1G.product.series[0].asin,
-				name: B017V4IM1G.product.series[0].title,
-				position: B017V4IM1G.product.series[0].sequence
-			},
-			seriesSecondary: {
-				asin: B017V4IM1G.product.series[1].asin,
-				name: B017V4IM1G.product.series[1].title,
-				position: B017V4IM1G.product.series[1].sequence
-			}
-		}
+		const description =
+			'Harry Potter has never even heard of Hogwarts when the letters start dropping on the doormat at number four, Privet Drive. Addressed in green ink on yellowish parchment with a purple seal, they are swiftly confiscated by his grisly aunt and uncle....'
+		const image = 'https://m.media-amazon.com/images/I/91eopoUCjLL.jpg'
+		minimalParsed = setupMinimalParsed(B017V4IM1G.product, description, image)
 	})
 
 	it('returned the correct data', () => {
