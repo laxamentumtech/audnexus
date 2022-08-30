f7740514000692ae72746e6678643cd42569ac1d
djdembeck <dembeck.david@protonmail.com>
2022-08-30 15:48:42 -0500

    refactor(types): :recycle: type predicates could erroneously pass
    also add test coverage for all cases

--- src/config/typing/checkers.ts
+++ src/config/typing/checkers.ts
@@ -4,9 +4,10 @@ import { ChapterDocument } from '#config/models/Chapter'
 import { ApiChapter, Book } from '#config/typing/books'
 import { AuthorProfile } from '#config/typing/people'
 
+// Use name for validity checking since it is unique to authors
 export function isAuthorProfile(author: unknown): author is AuthorProfile {
 	if (!author) return false
-	return typeof author === 'object' && 'name' in author
+	return typeof author === 'object' && !('_id' in author) && 'name' in author
 }
 
 export function isAuthorDocument(author: unknown): author is AuthorDocument {
@@ -14,9 +15,10 @@ export function isAuthorDocument(author: unknown): author is AuthorDocument {
 	return typeof author === 'object' && '_id' in author && 'name' in author
 }
 
+// Use title for validity checking since it is unique to books
 export function isBook(book: unknown): book is Book {
 	if (!book) return false
-	return typeof book === 'object' && 'title' in book
+	return typeof book === 'object' && !('_id' in book) && 'title' in book
 }
 
 export function isBookDocument(book: unknown): book is BookDocument {
@@ -24,9 +26,10 @@ export function isBookDocument(book: unknown): book is BookDocument {
 	return typeof book === 'object' && '_id' in book && 'title' in book
 }
 
+// Use runtimeLengthMs for validity checking since it is unique to chapters
 export function isChapter(chapter: unknown): chapter is ApiChapter {
 	if (!chapter) return false
-	return typeof chapter === 'object' && 'runtimeLengthMs' in chapter
+	return typeof chapter === 'object' && !('_id' in chapter) && 'runtimeLengthMs' in chapter
 }
 
 export function isChapterDocument(chapter: unknown): chapter is ChapterDocument {
