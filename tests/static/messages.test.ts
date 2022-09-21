ba36f4d066d2a46d88e0f54770c1943ae05c1c78
djdembeck <dembeck.david@protonmail.com>
2022-09-21 15:59:01 -0500

    test: :white_check_mark: add test for `messages.ts`

---
+++ tests/static/messages.test.ts
@@ -0,0 +1,23 @@
+import {
+	MessageDeleted,
+	MessageNoChapters,
+	MessageNoSearchParams,
+	MessageNotFoundInDb
+} from '#static/messages'
+
+const asin = 'B07JZ5ZQZQ'
+
+describe('Message output should be', () => {
+	test('MessageDeleted', () => {
+		expect(MessageDeleted(asin)).toBe(`${asin} deleted`)
+	})
+	test('MessageNoChapters', () => {
+		expect(MessageNoChapters(asin)).toBe(`${asin} has no chapters`)
+	})
+	test('MessageNoSearchParams', () => {
+		expect(MessageNoSearchParams).toBe('No search params provided')
+	})
+	test('MessageNotFoundInDb', () => {
+		expect(MessageNotFoundInDb(asin)).toBe(`${asin} not found in the database`)
+	})
+})
