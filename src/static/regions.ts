0b3ea23ec71967f062f703832da773c4d3602108
djdembeck <dembeck.david@protonmail.com>
2022-09-27 16:57:11 -0500

    refactor: :recycle: add chapter string to regions; rename to use single object; export type

--- src/static/regions.ts
+++ src/static/regions.ts
@@ -1,19 +1,78 @@
 // Regions and their TLDs
 
-import { AudibleRegion } from '#config/typing/audible'
+interface Region {
+	strings: {
+		[key: string]: string
+	}
+	tld: string
+}
+
+export interface Regions {
+	[key: string]: Region
+}
 
 // https://help.audible.com/s/article/what-is-an-audible-marketplace-and-which-is-best-for-me
-export const regionTLDs: AudibleRegion = {
-	au: 'com.au',
-	ca: 'ca',
-	de: 'de',
-	es: 'es',
-	fr: 'fr',
-	in: 'in',
-	it: 'it',
-	jp: 'co.jp',
-	us: 'com',
-	uk: 'co.uk'
+export const regions: Regions = {
+	au: {
+		strings: {
+			chapterName: 'Chapter'
+		},
+		tld: 'com.au'
+	},
+	ca: {
+		strings: {
+			chapterName: 'Chapter'
+		},
+		tld: 'ca'
+	},
+	de: {
+		strings: {
+			chapterName: 'Kapitel'
+		},
+		tld: 'de'
+	},
+	es: {
+		strings: {
+			chapterName: 'Capítulo'
+		},
+		tld: 'es'
+	},
+	fr: {
+		strings: {
+			chapterName: 'Chapitre'
+		},
+		tld: 'fr'
+	},
+	in: {
+		strings: {
+			chapterName: 'Chapter'
+		},
+		tld: 'in'
+	},
+	it: {
+		strings: {
+			chapterName: 'Capitolo'
+		},
+		tld: 'it'
+	},
+	jp: {
+		strings: {
+			chapterName: '章'
+		},
+		tld: 'co.jp'
+	},
+	us: {
+		strings: {
+			chapterName: 'Chapter'
+		},
+		tld: 'com'
+	},
+	uk: {
+		strings: {
+			chapterName: 'Chapter'
+		},
+		tld: 'co.uk'
+	}
 }
 
 // Regex should only match 2-letter lowercase ISO 3166-1 alpha-2 country codes
