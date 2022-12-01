096e7bd50e6fbad838081744679e75366080dab4
djdembeck <dembeck.david@protonmail.com>
2022-12-01 16:39:44 -0600

    test(helper): :white_check_mark: update book helper tests for recent genre changes

--- tests/helpers/books/audible/StitchHelper.test.ts
+++ tests/helpers/books/audible/StitchHelper.test.ts
@@ -1,8 +1,13 @@
+jest.mock('#helpers/books/audible/ApiHelper')
+jest.mock('#helpers/utils/shared')
+jest.mock('#helpers/books/audible/ScrapeHelper')
+
 import { AudibleProduct } from '#config/typing/audible'
 import { Book } from '#config/typing/books'
 import ApiHelper from '#helpers/books/audible/ApiHelper'
 import StitchHelper from '#helpers/books/audible/StitchHelper'
-import { ErrorMessageHTTPFetch, ErrorMessageParse } from '#static/messages'
+import { ErrorMessageHTTPFetch, ErrorMessageParse, ErrorMessageRequiredKey } from '#static/messages'
+import * as cheerio from 'cheerio'
 import {
 	apiResponse,
 	genresObject,
@@ -13,23 +18,36 @@ import {
 
 let asin: string
 let helper: StitchHelper
+let mockApiResponse: AudibleProduct
+let mockHTMLResponse: cheerio.CheerioAPI
 let region: string
+const deepCopy = (obj: Object) => JSON.parse(JSON.stringify(obj))
 
 beforeEach(() => {
 	asin = 'B079LRSMNN'
 	region = 'us'
+	mockApiResponse = deepCopy(apiResponse)
+	mockHTMLResponse = cheerio.load(htmlResponse)
 	// Set up helpers
 	helper = new StitchHelper(asin, region)
 })
 
 describe('StitchHelper should', () => {
 	beforeEach(() => {
-		jest.spyOn(helper.apiHelper, 'fetchBook').mockImplementation(() => Promise.resolve(apiResponse))
+		// Set up spys
+		jest.spyOn(helper.apiHelper, 'hasRequiredKeys').mockReturnValue({ isValid: true, message: '' })
 		jest
-			.spyOn(global, 'fetch')
-			.mockImplementationOnce(() =>
-				Promise.resolve({ ok: true, status: 200, text: () => htmlResponse } as unknown as Response)
-			)
+			.spyOn(helper.apiHelper, 'fetchBook')
+			.mockImplementation(() => Promise.resolve(mockApiResponse))
+		jest
+			.spyOn(helper.apiHelper, 'parseResponse')
+			.mockImplementation(() => Promise.resolve(parsedBook))
+		jest
+			.spyOn(helper.scrapeHelper, 'fetchBook')
+			.mockImplementation(() => Promise.resolve(mockHTMLResponse))
+		jest
+			.spyOn(helper.scrapeHelper, 'parseResponse')
+			.mockImplementation(() => Promise.resolve(genresObject))
 	})
 
 	test('setup constructor correctly', () => {
@@ -39,7 +57,7 @@ describe('StitchHelper should', () => {
 
 	test('fetch API source', async () => {
 		await helper.fetchApiBook()
-		expect(helper.apiResponse).toEqual(apiResponse)
+		expect(helper.apiResponse).toEqual(mockApiResponse)
 	})
 
 	test('parse API source', async () => {
@@ -58,40 +76,53 @@ describe('StitchHelper should', () => {
 		const proccessed = await helper.process()
 
 		expect(proccessed).toEqual(parsedBookWithGenres)
-		expect(helper.apiResponse).toEqual(apiResponse)
+		expect(helper.apiResponse).toEqual(mockApiResponse)
 		expect(helper.apiParsed).toEqual(parsedBook)
 	})
 })
 
 describe('SitchHelper should handle fallback', () => {
+	beforeEach(() => {
+		// Set up spys
+		jest.spyOn(helper.apiHelper, 'hasRequiredKeys').mockReturnValue({ isValid: true, message: '' })
+		jest
+			.spyOn(helper.apiHelper, 'parseResponse')
+			.mockImplementation(() => Promise.resolve(parsedBook))
+		jest
+			.spyOn(helper.scrapeHelper, 'parseResponse')
+			.mockImplementation(() => Promise.resolve(genresObject))
+		jest
+			.spyOn(helper.sharedHelper, 'sortObjectByKeys')
+			.mockImplementation(() => parsedBookWithGenres)
+	})
+
 	test('and includeGenres properly', async () => {
-		const obj = { data: apiResponse }
+		const obj = { data: mockApiResponse }
 		obj.data.product.category_ladders = []
 		jest.spyOn(helper.apiHelper, 'fetchBook').mockImplementation(() => Promise.resolve(obj.data))
-		jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
-			Promise.resolve({
-				ok: true,
-				status: 200,
-				text: () => htmlResponse
-			} as unknown as Response)
-		)
 		await helper.process()
 		expect(helper.scraperParsed).toEqual(genresObject)
 	})
 
 	test('and include no genres', async () => {
-		const obj = { data: apiResponse }
+		const obj = { data: mockApiResponse }
+		// Mock no genres from api
 		obj.data.product.category_ladders = []
+		// Mock no genres from scrape
+		const parsed = deepCopy(parsedBook)
+		delete parsed.genres
+		// Set spys
 		jest.spyOn(helper.apiHelper, 'fetchBook').mockImplementation(() => Promise.resolve(obj.data))
-		jest.spyOn(global, 'fetch').mockImplementationOnce(() =>
-			Promise.resolve({
-				ok: true,
-				status: 200
-			} as unknown as Response)
-		)
-		parsedBook.genres = []
+		jest
+			.spyOn(helper.scrapeHelper, 'fetchBook')
+			.mockImplementation(() => Promise.resolve(undefined))
+		jest
+			.spyOn(helper.scrapeHelper, 'parseResponse')
+			.mockImplementation(() => Promise.resolve(undefined))
+		jest.spyOn(helper.apiHelper, 'parseResponse').mockImplementation(() => Promise.resolve(parsed))
+
 		const processed = await helper.process()
-		expect(processed).toEqual(parsedBook)
+		expect(processed).toEqual(parsed)
 		expect(helper.scraperParsed).toBeUndefined()
 	})
 })
@@ -100,6 +131,14 @@ describe('StitchHelper should throw error when', () => {
 	beforeEach(() => {
 		// Mock Fetch to fail
 		jest.spyOn(global, 'fetch').mockImplementation(() => Promise.reject({ status: 400, ok: false }))
+		jest
+			.spyOn(helper.apiHelper, 'fetchBook')
+			.mockRejectedValue(new Error(ErrorMessageHTTPFetch(asin, 400, 'Audible API')))
+		jest.spyOn(helper.scrapeHelper, 'fetchBook').mockResolvedValue(undefined)
+		jest.spyOn(helper.scrapeHelper, 'parseResponse').mockResolvedValue(undefined)
+		jest
+			.spyOn(helper.apiHelper, 'parseResponse')
+			.mockRejectedValue(new Error(ErrorMessageParse(asin, 'Audible API')))
 	})
 
 	test('fetching api book data fails completely', async () => {
@@ -142,6 +181,10 @@ describe('StitchHelper should throw error when', () => {
 
 	test('processing book fails', async () => {
 		jest.spyOn(helper, 'fetchApiBook').mockImplementation()
+		jest.spyOn(helper.apiHelper, 'hasRequiredKeys').mockReturnValue({
+			isValid: false,
+			message: ErrorMessageRequiredKey(asin, 'authors', 'exist')
+		})
 		helper.apiHelper.inputJson = { asin: 'B07JZQZQZQ' } as unknown as AudibleProduct['product']
 		await expect(helper.process()).rejects.toThrowError(
 			`Required key 'authors' does not exist in Audible API response for ASIN ${asin}`
