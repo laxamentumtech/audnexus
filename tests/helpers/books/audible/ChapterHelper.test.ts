6eb12b0503de4e8be8c96b017e85c91c02014573
djdembeck <dembeck.david@protonmail.com>
2023-01-25 16:30:22 -0600

    feat: :alien: chapters no longer need auth
    remove deps, env vars, and documentation sections related to it

--- tests/helpers/books/audible/ChapterHelper.test.ts
+++ tests/helpers/books/audible/ChapterHelper.test.ts
@@ -17,8 +17,6 @@ beforeEach(() => {
 describe('ChapterHelper should', () => {
 	test('setup constructor correctly', () => {
 		expect(helper.asin).toBe(asin)
-		expect(helper.adpToken).toBeDefined()
-		expect(helper.privateKey).toBeDefined()
 		expect(helper.reqUrl).toBe(
 			`https://api.audible.com/1.0/content/${asin}/metadata?response_groups=chapter_info`
 		)
@@ -35,10 +33,6 @@ describe('ChapterHelper should', () => {
 		expect(helper.chapterTitleCleanup('Chapter 1.')).toBe('Chapter 1')
 		// Title with just a number is changed
 		expect(helper.chapterTitleCleanup('123')).toBe('Chapter 123')
-	})
-
-	test('sign request', () => {
-		expect(helper.signRequest(helper.adpToken, helper.privateKey)).toBeDefined()
 	})
 
 	test('fetch chapters', async () => {
@@ -78,21 +72,6 @@ describe('ChapterHelper should throw error when', () => {
 		expect(() => helper.getFinalData()).toThrowError('No input data')
 	})
 
-	const OLD_ENV = process.env
-
-	test('missing environment vars', () => {
-		// Set environment variables
-		process.env = { ...OLD_ENV }
-		process.env.ADP_TOKEN = undefined
-		process.env.PRIVATE_KEY = undefined
-		// setup function to fail if environment variables are missing
-		const bad_helper = function () {
-			new ChapterHelper(asin, region)
-		}
-		expect(bad_helper).toThrowError('Missing environment variable(s): ADP_TOKEN or PRIVATE_KEY')
-		// Restore environment
-		process.env = OLD_ENV
-	})
 	test('chapter missing required keys', async () => {
 		await expect(
 			helper.parseResponse({
